3890fc2e3473b89b9132c317edc8aae9011d14a0
Faizal (Zayd) Hussain <faizal.hussain33@gmail.com>
2018-10-15 23:12:28 -0400

    Fix port decleration

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const express = require('express'),
 
     path = require('path'),
     log = console.log,
-    PORT = process.env.PORT || 8080;
+    PORT = server.listen(process.env.PORT || 8080);
 
 app.use(express.static(path.resolve(__dirname + './../build')));
 
